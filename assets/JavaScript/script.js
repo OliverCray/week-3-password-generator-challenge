d6e260c8f089bb74e4c57db244e25006c31f588d
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-04-24 13:16:34 +0100

    reverted while loops back to if statements

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -11,12 +11,13 @@ var charSpecial = ["\u0020","\u0021","\u0022","\u0023","\u0024","\u0025","\u0026
 function generatePassword() {
   var passwordLength = Number(prompt("Choose a length for your password between 8 and 128 characters"))
   // Limit accepted inputs to between 8 and 128 inclusive and reject non numerical inputs
-  while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     window.alert("Invalid input, please choose a number between 8 and 128")
-    var passwordLength = Number(prompt("Choose a length for your password between 8 and 128 characters"))
+    return
   } 
   console.log("Password length: " + passwordLength)
   
+  // Choices for character types to be used in password
   var choiceLower = confirm("Would you like your password to include lowercase characters?")
   console.log("Inludes lowercase characters: " + choiceLower)
   var choiceUpper = confirm("Would you like your password to include uppercase characters?")
@@ -26,13 +27,16 @@ function generatePassword() {
   var choiceSpecial = confirm("Would you like your password to include special characters?")
   console.log("Inludes special characters: " + choiceSpecial)
 
-  while (!choiceLower && !choiceUpper && !choiceNum && !choiceSpecial) {
+  // Re-prompt the user if they select no for all character types
+  if (!choiceLower && !choiceUpper && !choiceNum && !choiceSpecial) {
     window.alert("Please choose at least one character type")
-    var choiceLower = confirm("Would you like your password to include lowercase characters?")
-    var choiceUpper = confirm("Would you like your password to include uppercase characters?")
-    var choiceNum = confirm("Would you like your password to include numbers?")
-    var choiceSpecial = confirm("Would you like your password to include special characters?")
+    return
   }
+
+  // Array to hold the password
+  var charTypes = []
+
+
 }
 
 // Write password to the #password input

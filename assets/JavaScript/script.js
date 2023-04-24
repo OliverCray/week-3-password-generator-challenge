2eec356f0063d87bd5c70ff1e563fd3c5647acaf
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-04-24 12:52:43 +0100

    added options for character types

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,9 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Character types 
+var charLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var charUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var charNum = ["0","1","2","3","4","5","6","7","8","9"]
+var charSpecial = ["\u0020","\u0021","\u0022","\u0023","\u0024","\u0025","\u0026","\u0027","\u0028","\u0029","\u002A","\u002B","\u002C","\u002D","\u002E","\u002F","\u003A","\u003B","\u003C","\u003D","\u003E","\u003F","\u0040","\u005B","\u005C","\u005D","\u005E","\u005F","\u0060","\u007B","\u007C","\u007D","\u007E"]
+
 // Password generation prompts
 function generatePassword() {
   var passwordLength = Number(prompt("Choose a length for your password between 8 and 128 characters"))
+  // Limit accepted inputs to between 8 and 128 inclusive and reject non numerical inputs
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Invalid input, please choose a number between 8 and 128")
     return
@@ -12,7 +19,15 @@ function generatePassword() {
     return
   }
   console.log("Password length: " + passwordLength)
-
+  
+  var choiceLower = confirm("Would you like your password to include lowercase characters?")
+  console.log(choiceLower)
+  var choiceUpper = confirm("Would you like your password to include uppercase characters?")
+  console.log(choiceUpper)
+  var choiceNum = confirm("Would you like your password to include numbers?")
+  console.log(choiceNum)
+  var choiceSpecial = confirm("Would you like your password to include special characters?")
+  console.log(choiceSpecial)
 }
 
 // Write password to the #password input

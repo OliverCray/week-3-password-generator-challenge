102706d6e521476a37b702db8bddf2fc832336a3
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-04-24 11:39:13 +0100

    created generatePassword function

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Password generation prompts
+function generatePassword() {
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

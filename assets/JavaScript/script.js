20ad36e3d24214c78e10afd43ae7df0e36226434
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-04-24 14:05:50 +0100

    added button to copy the password to clipboard

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy")
 
 // Character types 
 var charLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -72,3 +73,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add event listener to copy button
+copyBtn.addEventListener("click", copyPassword)

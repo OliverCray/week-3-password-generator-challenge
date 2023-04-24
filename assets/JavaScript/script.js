d439c4c1b4779d6a953c6d7a30671925aa461070
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-04-24 15:50:45 +0100

    placed char types in an object, adjusted var names

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -2,11 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector("#copy")
 
-// Character types 
-var charLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var charUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var charNum = ["0","1","2","3","4","5","6","7","8","9"]
-var charSpecial = ["\u0020","\u0021","\u0022","\u0023","\u0024","\u0025","\u0026","\u0027","\u0028","\u0029","\u002A","\u002B","\u002C","\u002D","\u002E","\u002F","\u003A","\u003B","\u003C","\u003D","\u003E","\u003F","\u0040","\u005B","\u005C","\u005D","\u005E","\u005F","\u0060","\u007B","\u007C","\u007D","\u007E"]
+// Character types object
+var charTypes = {
+  charLower: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  charUpper: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  charNum : ["0","1","2","3","4","5","6","7","8","9"],
+  charSpecial: ["\u0020","\u0021","\u0022","\u0023","\u0024","\u0025","\u0026","\u0027","\u0028","\u0029","\u002A","\u002B","\u002C","\u002D","\u002E","\u002F","\u003A","\u003B","\u003C","\u003D","\u003E","\u003F","\u0040","\u005B","\u005C","\u005D","\u005E","\u005F","\u0060","\u007B","\u007C","\u007D","\u007E"]
+}
 
 // Password generation prompts
 function generatePassword() {
@@ -35,27 +37,27 @@ function generatePassword() {
   }
 
   // Stores the chosen character types
-  var charTypes = []
+  var charChoice = []
   
   if (choiceLower) {
-    charTypes = charTypes.concat(charLower)  
+    charChoice = charChoice.concat(charTypes.charLower)  
   } 
   if (choiceUpper) {
-    charTypes = charTypes.concat(charUpper)
+    charChoice = charChoice.concat(charTypes.charUpper)
   } 
   if (choiceNum) {
-    charTypes = charTypes.concat(charNum)
+    charChoice = charChoice.concat(charTypes.charNum)
   } 
   if (choiceSpecial) {
-    charTypes = charTypes.concat(charSpecial)
+    charChoice = charChoice.concat(charTypes.charSpecial)
   }
-  console.log("Selected charTpes: " + charTypes)  
+  console.log("Selected charTypes: " + charChoice)  
 
   var generatedPassword = ""
 
   // Generates password from chosen criteria
   for (var i = 0; i < passwordLength; i++) {
-    generatedPassword = generatedPassword + charTypes[Math.floor(Math.random() * charTypes.length)]
+    generatedPassword = generatedPassword + charChoice[Math.floor(Math.random() * charChoice.length)]
   }
 
   console.log(generatedPassword)

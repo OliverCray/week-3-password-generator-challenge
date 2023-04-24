469823dfa482be054075a5e8043630beda335dbc
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-04-24 12:05:20 +0100

    created passwordLength var with input validation

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -3,6 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Password generation prompts
 function generatePassword() {
+  var passwordLength = Number(prompt("Choose a length for your password between 8 and 128 characters"))
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Invalid input, please choose a number between 8 and 128")
+    return
+  } else if (isNaN(passwordLength)) {
+    window.alert("Invalid input, please choose a number between 8 and 128")
+    return
+  }
+  console.log("Password length: " + passwordLength)
 
 }
 
